b55ccb5b6f79cd50da214efef4280a98fba071fa
Warren Halderman <halderman.warren@gmail.com>
2025-05-19 21:09:04 +0900

    up

--- mod/audio.nix
+++ mod/audio.nix
@@ -2,7 +2,7 @@
 
 {
 	# Enable sound with pipewire.
-	services.pulseaudio.enable = false;
+	hardware.pulseaudio.enable = false;
 	# PulseAudio and PipeWire use rtkit to acquire realtime priority.
 	security.rtkit.enable = true; 
 	services.pipewire = {
